48e0886e890970a745ef706b057785cee6366c7f
Charles <charlesfaye99@gmail.com>
2025-07-10 22:21:13 +0100

    Adding script of typed.js library

--- mon-portfolio/src/main.js
+++ mon-portfolio/src/main.js
@@ -1,1 +1,12 @@
 import { mobileNavbar, listIcon, anchorElements } from "./nav.js";
+
+
+ document.addEventListener('DOMContentLoaded', function () {
+    new Typed("#typed", {
+      strings: ["Développeur Web.", "Freelance.", "Passionné de code."],
+      typeSpeed: 100,
+      backSpeed: 50,
+      backDelay: 2000,
+      loop: true
+    });
+  });
