b32c82d19ea2c103fd5e02460482f33c7408a1c7
Charles <charlesfaye99@gmail.com>
2025-08-01 12:42:02 +0100

    Change the variable names and rewrite certain syntax functions

--- mon-portfolio/src/main.js
+++ mon-portfolio/src/main.js
@@ -1,7 +1,5 @@
 const navmenu = document.getElementById('navmenu');
 const listIcon = document.querySelector('#list-icon > i');
-
-
 /**
  * Toggles the visibility and animation of the mobile navigation bar.
  * 
@@ -14,56 +12,46 @@ const listIcon = document.querySelector('#list-icon > i');
  * @returns {void}
  */
 const toggleNavbar = () => {
-    navmenu.classList.toggle('hidden');
-    listIcon.classList.toggle('bi-list');
-    listIcon.classList.toggle('bi-x');
-    if (listIcon.classList.contains('bi-x')) {
-        navmenu.classList.add('animate-fade-in');
-        navmenu.classList.remove('animate-fade-out');
-    }
-    else {
-        navmenu.classList.remove('animate-fade-in');
-        navmenu.classList.add('animate-fade-out');
-    }
-}
-
-
+  navmenu.classList.toggle('hidden');
+  listIcon.classList.toggle('bi-list');
+  listIcon.classList.toggle('bi-x');
+  navmenu.classList.toggle('animate-fade-in', listIcon.classList.contains('bi-x'));
+  navmenu.classList.toggle('animate-fade-out', !listIcon.classList.contains('bi-x'));
+};
 listIcon.addEventListener('click', toggleNavbar);
 
+const elHTML = document.querySelector('[data-skill="HTML"]');
+const elCSS = document.querySelector('[data-skill="CSS"]');
+const elJS = document.querySelector('[data-skill="JavaScript"]');
+const elTailwind = document.querySelector('[data-skill="TailwindCSS"]');
+const elWP = document.querySelector('[data-skill="Wordpress"]');
+const elMobile = document.querySelector('[data-skill="mobile"]');
+const elDev = document.querySelector('[data-skill="dev-tools"]');
+const elVC = document.querySelector('[data-skill="version-control"]');
+const elDeploy = document.querySelector('[data-skill="deployment-hosting"]');
+const elSEO = document.querySelector('[data-skill="seo-performance"]');
 
+const barHTML = document.getElementById('html-bar');
+const barCSS = document.getElementById('css-bar');
+const barJS = document.getElementById('js-bar');
+const barTailwind = document.getElementById('tailwind-bar');
+const barWP = document.getElementById('wordpress-bar');
+const barMobile = document.getElementById('mobile-bar');
+const barDev = document.getElementById('dev-tools-bar');
+const barVC = document.getElementById('version-control-bar');
+const barDeploy = document.getElementById('deployment-hosting-bar');
+const barSEO = document.getElementById('seo-performance-bar');
 
-
-const skillHTML = document.querySelector('[data-skill="HTML"]');
-const skillCSS = document.querySelector('[data-skill="CSS"]');
-const skillJavaScript = document.querySelector('[data-skill="JavaScript"]');
-const skillTailwind = document.querySelector('[data-skill="TailwindCSS"]');
-const skillWordpress = document.querySelector('[data-skill="Wordpress"]');
-const skillMobile = document.querySelector('[data-skill="mobile"]');
-const skillDevTools = document.querySelector('[data-skill="dev-tools"]');
-const skillVersionControl = document.querySelector('[data-skill="version-control"]');
-const skillDeploymentHosting = document.querySelector('[data-skill="deployment-hosting"]');
-const skillSeoPerformance = document.querySelector('[data-skill="seo-performance"]');
-const htmlBar = document.getElementById('html-bar');
-const cssBar = document.getElementById('css-bar');
-const javaScriptBar = document.getElementById('js-bar');
-const tailwindcssBar = document.getElementById('tailwind-bar');
-const wordpressBar = document.getElementById('wordpress-bar');
-const mobileBar = document.getElementById('mobile-bar');
-const devToolsBar = document.getElementById('dev-tools-bar');
-const versionControlBar = document.getElementById('version-control-bar');
-const deploymentHostingBar = document.getElementById('deployment-hosting-bar');
-const seoPerformanceBar = document.getElementById('seo-performance-bar');
-
-let htmlCurrentValue = document.getElementById("html-bar").getAttribute('value');
-let cssCurrentValue = document.getElementById('css-bar').getAttribute('value');
-let jsCurrentValue = document.getElementById('js-bar').getAttribute('value');
-let tailwindCurrentValue = document.getElementById('tailwind-bar').getAttribute('value');
-let wordpressCurrentValue = document.getElementById('wordpress-bar').getAttribute('value');
-let mobileCurrentValue = document.getElementById('mobile-bar').getAttribute('value');
-let devToolsCurrentValue = document.getElementById('dev-tools-bar').getAttribute('value');
-let versionControlCurrentValue = document.getElementById('version-control-bar').getAttribute('value');
-let deploymentHostingCurrentValue = document.getElementById('deployment-hosting-bar').getAttribute('value');
-let seoPerformanceCurrentValue = document.getElementById('seo-performance-bar').getAttribute('value');
+let htmlVal = document.getElementById("html-bar").getAttribute('value');
+let cssVal = document.getElementById('css-bar').getAttribute('value');
+let jsVal = document.getElementById('js-bar').getAttribute('value');
+let tailwindVal = document.getElementById('tailwind-bar').getAttribute('value');
+let wpVal = document.getElementById('wordpress-bar').getAttribute('value');
+let mobileVal = document.getElementById('mobile-bar').getAttribute('value');
+let devVal = document.getElementById('dev-tools-bar').getAttribute('value');
+let vcVal = document.getElementById('version-control-bar').getAttribute('value');
+let deployVal = document.getElementById('deployment-hosting-bar').getAttribute('value');
+let seoVal = document.getElementById('seo-performance-bar').getAttribute('value');
 
 const skills = [
     {
@@ -107,21 +95,18 @@ const skills = [
       level: 60
     }
 ];
-
-
 const skillElements = {
-    HTML5: skillHTML,
-    CSS3: skillCSS,
-    JavaScript: skillJavaScript,
-    TailwindCSS: skillTailwind,
-    Wordpress: skillWordpress,
-    "Dev Mobile": skillMobile,
-    "Outils de développement (Vite.js, npm, Bash, ESLint, ...)": skillDevTools,
-    "Contrôle de version (Git, GitHub, ...)": skillVersionControl,
-    "Déploiement & Hébergement (Vercel, Netlify, etc...)": skillDeploymentHosting,
-    "SEO & performance (Lighthouse, Google Analytics 4, ...)": skillSeoPerformance
+    HTML5: elHTML,
+    CSS3: elCSS,
+    JavaScript: elJS,
+    TailwindCSS: elTailwind,
+    Wordpress: elWP,
+    "Dev Mobile": elMobile,
+    "Outils de développement (Vite.js, npm, Bash, ESLint, ...)": elDev,
+    "Contrôle de version (Git, GitHub, ...)": elVC,
+    "Déploiement & Hébergement (Vercel, Netlify, etc...)": elDeploy,
+    "SEO & performance (Lighthouse, Google Analytics 4, ...)": elSEO
 };
-
 /**
  * Populates the skill containers with the skill name and level percentage.
  * For each skill in the `skills` array, creates a paragraph and span element,
@@ -145,36 +130,31 @@ const fillProgressBars = () => {
         }
     }
 };
-
 fillProgressBars()
-
-
 const barElements = {
-    HTML5: htmlBar,
-    CSS3: cssBar,
-    JavaScript: javaScriptBar,
-    TailwindCSS: tailwindcssBar,
-    Wordpress: wordpressBar,
-    "Dev Mobile": mobileBar,
-    "Outils de développement (Vite.js, npm, Bash, ESLint, ...)": devToolsBar,
-    "Contrôle de version (Git, GitHub, ...)": versionControlBar,
-    "Déploiement & Hébergement (Vercel, Netlify, etc...)": deploymentHostingBar,
-    "SEO & performance (Lighthouse, Google Analytics 4, ...)": seoPerformanceBar
+    HTML5: barHTML,
+    CSS3: barCSS,
+    JavaScript: barJS,
+    TailwindCSS: barTailwind,
+    Wordpress: barWP,
+    "Dev Mobile": barMobile,
+    "Outils de développement (Vite.js, npm, Bash, ESLint, ...)": barDev,
+    "Contrôle de version (Git, GitHub, ...)": barVC,
+    "Déploiement & Hébergement (Vercel, Netlify, etc...)": barDeploy,
+    "SEO & performance (Lighthouse, Google Analytics 4, ...)": barSEO
 };
-
 let barValues = {
-    HTML5: Number(htmlCurrentValue),
-    CSS3: Number(cssCurrentValue),
-    JavaScript: Number(jsCurrentValue),
-    TailwindCSS: Number(tailwindCurrentValue),
-    Wordpress: Number(wordpressCurrentValue),
-    "Dev Mobile": Number(mobileCurrentValue),
-    "Outils de développement (Vite.js, npm, Bash, ESLint, ...)": Number(devToolsCurrentValue),
-    "Contrôle de version (Git, GitHub, ...)": Number(versionControlCurrentValue),
-    "Déploiement & Hébergement (Vercel, Netlify, etc...)": Number(deploymentHostingCurrentValue),
-    "SEO & performance (Lighthouse, Google Analytics 4, ...)": Number(seoPerformanceCurrentValue)
+    HTML5: Number(htmlVal),
+    CSS3: Number(cssVal),
+    JavaScript: Number(jsVal),
+    TailwindCSS: Number(tailwindVal),
+    Wordpress: Number(wpVal),
+    "Dev Mobile": Number(mobileVal),
+    "Outils de développement (Vite.js, npm, Bash, ESLint, ...)": Number(devVal),
+    "Contrôle de version (Git, GitHub, ...)": Number(vcVal),
+    "Déploiement & Hébergement (Vercel, Netlify, etc...)": Number(deployVal),
+    "SEO & performance (Lighthouse, Google Analytics 4, ...)": Number(seoVal)
 };
-
 /**
  * Animates the progress bars for each skill by incrementally updating their values
  * until they reach the target skill level, creating a smooth animation effect.
@@ -202,12 +182,8 @@ const simulateSmoothAnimation = () => {
         }, 20);
     });
 };
-
-
-
 const projectContainer = document.querySelector('.project');
 let iso;
-
 imagesLoaded(projectContainer, function() {
   iso = new Isotope(projectContainer, {
   itemSelector: '.project-item',
@@ -217,11 +193,7 @@ imagesLoaded(projectContainer, function() {
   }
 });
 });
-
-
 const filterButtons = document.querySelectorAll('#filter-container li');
-
-
 filterButtons.forEach(function(button) {
   button.addEventListener('click', function() {
     this.parentElement.querySelector('.filter-active').classList.remove('filter-active');
@@ -238,15 +210,10 @@ filterButtons.forEach(function(button) {
     selector: '.glightbox'
   });
 
-
   const dateElement = document.querySelector('.date');
   const currentDate = new Date();
   dateElement.textContent = currentDate.getFullYear();
-
-
-
   let navmenulinks = document.querySelectorAll('#navmenu a');
-  
   /**
    * Updates the active state of navigation menu links based on the current scroll position.
    * Highlights the link corresponding to the section currently in view and adds a 'text-white' class to its icon.
@@ -271,40 +238,32 @@ filterButtons.forEach(function(button) {
       }
     })
   }
-
 window.addEventListener('load', navmenuScrollspy);
 document.addEventListener('scroll', navmenuScrollspy);
-
-
-  let scrollTop = document.querySelector('.scroll-top');
-
-  /**
-   * Toggles the visibility of the scroll-to-top button based on the window's scroll position.
-   * Removes the 'opacity-0' class from the scrollTop element when scrolled more than 100px,
-   * otherwise adds the 'opacity-0' class to hide it.
-   *
-   * @function
-   */
+let scrollTop = document.querySelector('.scroll-top');
+/**
+  * Toggles the visibility of the scroll-to-top button based on the window's scroll position.
+  * Removes the 'opacity-0' class from the scrollTop element when scrolled more than 100px,
+  * otherwise adds the 'opacity-0' class to hide it.
+  *
+  * @function
+*/
   function toggleScrollTop() {
     if (scrollTop) {
       window.scrollY < 100 ? scrollTop.classList.add('opacity-0') : scrollTop.classList.remove('opacity-0');
     }
   }
-
-  scrollTop.addEventListener('click', (e) => {
+scrollTop.addEventListener('click', (e) => {
     e.preventDefault();
     window.scrollTo({
       top: 0,
       behavior: 'smooth'
     });
   });
-
-  window.addEventListener('load', toggleScrollTop);
-  document.addEventListener('scroll', toggleScrollTop);
-
-  const heroContent = document.querySelector('.hero-content');
-
-  const observer = new IntersectionObserver(entries => {
+window.addEventListener('load', toggleScrollTop);
+document.addEventListener('scroll', toggleScrollTop);
+const heroContent = document.querySelector('.hero-content');
+const observer = new IntersectionObserver(entries => {
     entries.forEach(entry => {
         entry.target.classList.remove('opacity-0');
         entry.target.classList.add('animate-zoom-out');
@@ -314,61 +273,45 @@ document.addEventListener('scroll', navmenuScrollspy);
 {
   threshold: 1
 })
-
 observer.observe(heroContent)
-
 const intersectElements = document.querySelectorAll('.intersect-element');
-
-
 const newObserver = new IntersectionObserver(entries => {
   entries.forEach(entry => {
-      if(entry.isIntersecting) {
-        entry.target.classList.remove('opacity-0');
-        entry.target.classList.add('animate-fadeInUp');
-        newObserver.unobserve(entry.target)
-      }
-  })
-},
-{
-  threshold: 0.1,
-  rootMargin: "0px 0px -10px 0px"
-});
-
-
+    if (entry.isIntersecting) {
+      entry.target.classList.remove('opacity-0');
+      entry.target.classList.add('animate-fadeInUp');
+      newObserver.unobserve(entry.target);
+    }
+  });
+}, { threshold: 0.1,
+    rootMargin: "0px 0px -10px 0px"
+ });
 document.addEventListener('DOMContentLoaded', () => {
   intersectElements.forEach(element => {
     newObserver.observe(element)
   });
 });
-
-
 const skillsSection = document.getElementById('skills');
-
 const skillsObserver = new IntersectionObserver(entries => {
   entries.forEach(entry => {
-    if (entry.isIntersecting) {
-      simulateSmoothAnimation()
-      entry.target.classList.remove('opacity-0');
-      skillsObserver.unobserve(entry.target)
-    }
-  })
+    if (!entry.isIntersecting) return;
+    simulateSmoothAnimation();
+    entry.target.classList.remove('opacity-0');
+    skillsObserver.unobserve(entry.target);
+  });
 });
-
 skillsObserver.observe(skillsSection);
-
-  /**
-   * Preloader
-   */
+/**
+* Preloader
+*/
   const preloader = document.querySelector('#preloader');
   if (preloader) {
     window.addEventListener('load', () => {
       preloader.remove();
     });
   }
-
 const html = document.documentElement;
 const sunIcon = document.querySelector('#dark-mode > i');
-
 const getTheme = localStorage.getItem('data');
 document.addEventListener('DOMContentLoaded', () => {
     if (getTheme === 'dark') {
@@ -379,28 +322,19 @@ document.addEventListener('DOMContentLoaded', () => {
         html.classList.remove('dark');
     }
 })
-
   /**
- * Toggles the website's theme between light and dark modes.
- * Updates the HTML element's class, changes the icon, and stores the current theme in localStorage.
- *
- * @function
- * @returns {void}
- */
+   * Toggles between dark and light mode for the application.
+   * - Adds or removes the 'dark' class on the root HTML element.
+   * - Updates the sunIcon's class to reflect the current mode (moon for dark, sun for light).
+   * - Persists the current mode ('dark' or 'light') in localStorage under the key 'data'.
+   *
+   * @function
+   * @returns {void}
+   */
   const toggleMode = () => {
-    const theme = html.getAttribute('class');
-    if (theme !== 'dark') {
-        html.classList.add('dark'); 
-        sunIcon.classList.remove('bi-brightness-low');
-        sunIcon.classList.add('bi-moon-stars');
-        localStorage.setItem('data', 'dark');
-    }
-    else {
-        html.classList.remove('dark');
-        sunIcon.classList.add('bi-brightness-low');
-        sunIcon.classList.remove('bi-moon-stars');
-        localStorage.setItem('data', 'light');
-    }
-  }
-
-  sunIcon.addEventListener('click', toggleMode);
+    const isDark = html.classList.toggle('dark');
+    sunIcon.classList.toggle('bi-moon-stars', isDark);
+    sunIcon.classList.toggle('bi-brightness-low', !isDark);
+    localStorage.setItem('data', isDark ? 'dark' : 'light');
+  };
+sunIcon.addEventListener('click', toggleMode);
